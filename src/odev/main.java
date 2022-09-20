862b3312419f84ef26042a37ed76cc9c39e0c33c
gulci <gulci@LAPTOP-KPNR185M>
2022-09-20 17:46:54 +0300

    deneme

---
+++ src/odev/main.java
@@ -0,0 +1,19 @@
+package odev;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Merhaba Java");
+		System.
+		out
+		.
+		println("Merhaba Java 2")
+		;
+		
+		//ekrana merhaba java 2 yazdırırken bilerek kodlarda düzensiz boşluklar bıraktım 
+		//bunu yapmamdaki amaç derste noktalı virgülden bahsedildiği için deneyerek ve görerek öğrenmekti
+	
+	}
+
+}
